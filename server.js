fda5ad4128ccb66dd35e182eaf18fe773e07cfac
Asbir Vkp <116637800+asbirvkp@users.noreply.github.com>
2024-11-01 14:53:43 +0530

    -

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? 
+      process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join('\n') : undefined
   })
 });
 
